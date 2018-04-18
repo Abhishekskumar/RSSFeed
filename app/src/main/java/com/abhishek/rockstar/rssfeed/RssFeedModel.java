700a799eb9478d7904d0a38dcefc8b6c564648b1
Abhishek <abhishekskumar69@gmail.com>
2018-04-18 05:47:18 +0530

    Initial commit

---
+++ app/src/main/java/com/abhishek/rockstar/rssfeed/RssFeedModel.java
@@ -0,0 +1,17 @@
+package com.abhishek.rockstar.rssfeed;
+
+/**
+ * Created by Admin on 18-04-2018.
+ */
+
+public class RssFeedModel {
+    public String title;
+    public String link;
+    public String description;
+    public RssFeedModel(String title, String link, String description) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+    }
+}
+
